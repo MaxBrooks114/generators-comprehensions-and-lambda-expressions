95f178293809891de2975b852c1ce4db91d2326d
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-05-14 15:46:16 -0400

    reading tags

--- filesearch.py
+++ filesearch.py
@@ -1,5 +1,6 @@
 import os
 import fnmatch
+import id3reader_p3 as id3reader
 
 
 def find_albums(root, artist_name):
@@ -16,12 +17,38 @@ def find_songs(albums):
         for song in os.listdir(album[0]):
             yield song
 
-
-album_list = find_albums("music", input("Artist please: "))
-song_list = find_songs(album_list)
 #
-# for a in album_list:
-#     print(a)
+# album_list = find_albums("music", input("Artist please: "))
+# song_list = find_songs(album_list)
+# #
+# # for a in album_list:
+# #     print(a)
+#
+# for s in song_list:
+#     print(s)
 
-for s in song_list:
-    print(s)
+
+def music_search(start_dir, extension):
+    for path, directories, files in os.walk(start_dir):
+        for file in fnmatch.filter(files, '*.{}'.format(extension)):
+            absolute_path = os.path.abspath(path)
+            yield os.path.join(absolute_path, file)
+
+
+music_files = music_search("music", 'emp3')
+
+error_list = []
+for f in music_files:
+    try:
+        id3r = id3reader.Reader(f)
+        print("Artist: {}, Album: {}, Track: {}, Song: {}".format(
+            id3r.get_value("performer"),
+            id3r.get_value("album"),
+            id3r.get_value("track"),
+            id3r.get_value("song")
+        ))
+    except OSError:
+        error_list.append(f)
+
+for error_file in error_list:
+    print(error_file)
