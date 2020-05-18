8e1df5bf59d8c81b769a82cb5facb95a3abfd1f6
Max Brooks <45081361+MaxBrooks114@users.noreply.github.com>
2020-05-18 15:14:33 -0400

    nested comprehensions

---
+++ nested_challenge.py
@@ -0,0 +1,7 @@
+# for i in range(1,11):
+#     for j in range(1,11):
+#         print(i, i * j)
+
+for times_tables in [(i, i * j) for j in range(1, 11) for i in range(1,11)]:
+
+    print(times_tables``)
